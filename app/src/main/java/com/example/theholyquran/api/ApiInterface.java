48545fad5b5d953bc3810c74e582327ce188c7b9
andi muh haikal lukman <113303662+itsdccal@users.noreply.github.com>
2024-05-27 00:27:55 +0800

    fix api

--- app/src/main/java/com/example/theholyquran/api/ApiInterface.java
+++ app/src/main/java/com/example/theholyquran/api/ApiInterface.java
@@ -1,5 +1,25 @@
 package com.example.theholyquran.api;
 
-public interface ApiInterface {
+import com.example.theholyquran.model.Cek;
+import com.example.theholyquran.model.Data;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface ApiInterface {
+    @GET("quran/id.indonesian")
+    Call<Data> getData();
+
+    @GET("quran/quran-uthmani")
+    Call<Cek> getDataArabic();
+
+    @GET("quran/{lang}")
+    Call<Cek> getCek(@Path("lang") String lang);
+
+    @GET("surah/1/id.indonesian")
+    Call<Cek> getSurahTranslation(/*@Path("id")int id*/);
+
+    // id.indonesian
 }
+
