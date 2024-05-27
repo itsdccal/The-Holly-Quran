261154e54d4c913d382098e3927c44035365e1f4
andi muh haikal lukman <113303662+itsdccal@users.noreply.github.com>
2024-05-27 21:09:28 +0800

    create activity surah

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,8 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
+    implementation ("com.github.florent37:expansionpanel:1.2.4")
+
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.okhttp3:okhttp:4.9.1")
