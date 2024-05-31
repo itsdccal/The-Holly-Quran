f07aa37005c2cfef3bceed39adcdfe629cb865e0
andi muh haikal lukman <113303662+itsdccal@users.noreply.github.com>
2024-05-31 22:24:15 +0800

    fix eror yeayyy

--- app/src/main/java/com/example/theholyquran/api/ApiClient.java
+++ app/src/main/java/com/example/theholyquran/api/ApiClient.java
@@ -7,7 +7,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASEURL = "http://api.alquran.cloud/v1/";
+    private static final String BASEURL = "https://api.alquran.cloud/v1/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getRetrofit() {
