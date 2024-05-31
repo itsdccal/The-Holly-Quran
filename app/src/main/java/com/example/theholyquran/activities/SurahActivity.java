ca06207b1ddc3dbe089fd23cf2ef250e64c27d0e
andi muh haikal lukman <113303662+itsdccal@users.noreply.github.com>
2024-06-01 00:15:04 +0800

    update ayat adapter

--- app/src/main/java/com/example/theholyquran/activities/SurahActivity.java
+++ app/src/main/java/com/example/theholyquran/activities/SurahActivity.java
@@ -5,8 +5,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.theholyquran.R;
+import com.example.theholyquran.adapter.AyatAdapter;
 import com.example.theholyquran.model.Ayat;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -34,20 +37,28 @@ public class SurahActivity extends AppCompatActivity {
         }.getType();
 
         TextView tvTitle = findViewById(R.id.tvTitle);
+        TextView tvSubTitle = findViewById(R.id.tvSubTitle);
+        TextView tvInfo = findViewById(R.id.tvInfo);
+
+        tvSubTitle.setText(jsonIndo);
+        tvSubTitle.findViewById(R.id.tvSubTitle);
+        tvSubTitle.setText(jsonIndo);
+
         tvTitle.setText(jsonTitle);
         tvTitle.findViewById(R.id.tvTitle);
         tvTitle.setText(jsonTitle);
+
 
         Gson gson = new Gson();
         List<Ayat> ayatList = gson.fromJson(json, type);
         List<Ayat> ayatListIndo = gson.fromJson(jsonIndo, type);
 
-//        RecyclerView rvAyat = findViewById(R.id.rvAyat);
-//        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-//        AyatAdapter ayatAdapter = new AyatAdapter(this, ayatList, ayatListIndo);
-//        rvAyat.setLayoutManager(layoutManager);
-//        rvAyat.setHasFixedSize(true);
-//        rvAyat.setAdapter(ayatAdapter);
+        RecyclerView rvAyat = findViewById(R.id.rvAyat);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        AyatAdapter ayatAdapter = new AyatAdapter(this, ayatList, ayatListIndo);
+        rvAyat.setLayoutManager(layoutManager);
+        rvAyat.setHasFixedSize(true);
+        rvAyat.setAdapter(ayatAdapter);
 
     }
 }
--- app/src/main/java/com/example/theholyquran/adapter/AyatAdapter.java
+++ app/src/main/java/com/example/theholyquran/adapter/AyatAdapter.java
@@ -1,4 +1,66 @@
 package com.example.theholyquran.adapter;
 
-public class AyatAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.theholyquran.R;
+import com.example.theholyquran.model.Ayat;
+
+import java.util.List;
+
+public class AyatAdapter extends RecyclerView.Adapter<AyatAdapter.Holder> {
+
+    private Context context;
+    private List<Ayat> list;
+    private List<Ayat> listIndo;
+
+    public AyatAdapter(Context context, List<Ayat> list, List<Ayat> listIndo) {
+        this.context = context;
+        this.list = list;
+        this.listIndo = listIndo;
+    }
+
+    @NonNull
+    @Override
+    public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        View view = LayoutInflater.from(context).inflate(R.layout.layout_ayat, parent, false);
+        return new Holder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Holder holder, int position) {
+        if (list != null && listIndo != null) {
+            Ayat ayat = list.get(position);
+            Ayat ayatIndo = listIndo.get(position);
+            if (ayat != null && ayatIndo != null) {
+                holder.tvNomorAyat.setText(ayat.getNumberInSurah());
+                holder.tvArabic.setText(ayat.getText());
+                holder.tvTerjemahan.setText(ayatIndo.getText());
+            }
+        }
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class Holder extends RecyclerView.ViewHolder {
+        TextView tvNomorAyat, tvArabic, tvTerjemahan;
+
+        public Holder(View itemView) {
+            super(itemView);
+            tvNomorAyat = itemView.findViewById(R.id.tvNomorAyat);
+            tvArabic = itemView.findViewById(R.id.tvArabic);
+            tvTerjemahan = itemView.findViewById(R.id.tvTerjemahan);
+        }
+    }
 }
